8afc8e2fdd167004ca2d74f927c1f4866917da38
kedar sedai <kedar.sedai05@gmail.com>
2021-05-18 22:08:45 +0545

    GCD & LCM

---
+++ lcmGcd.cpp
@@ -0,0 +1,41 @@
+// You are given a positive integer x. Find any such 2 positive integers a and b such that GCD(a,b)+LCM(a,b)=x.
+
+// As a reminder, GCD(a,b) is the greatest integer that divides both a and b. Similarly, LCM(a,b) is the smallest integer such that both a and b divide it.
+
+// It's guaranteed that the solution always exists. If there are several such pairs (a,b), you can output any of them.
+
+// Input
+// The first line contains a single integer t (1≤t≤100)  — the number of testcases.
+
+// Each testcase consists of one line containing a single integer, x (2≤x≤109).
+
+// Output
+// For each testcase, output a pair of positive integers a and b (1≤a,b≤109) such that GCD(a,b)+LCM(a,b)=x. It's guaranteed that the solution always exists. If there are several such pairs (a,b), you can output any of them.
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int n1, n2, gcd, lcm;
+
+  cout << "Enter two Number for GCD & LCM";
+  cin >> n1 >> n2;
+
+    gcd = 1;
+    lcm = 1;
+
+    for (int i = 1; i <= 1000; i++){
+        if((n1 % i == 0) && (n2 % i == 0)){
+            gcd = i;
+        }
+    }
+ 
+    lcm = (n1 * n2 ) / gcd;
+
+    cout << "GCD of two number " << n1 << " and " << n2 << " is: " << gcd;
+    cout << "LCM of Two Number is " << n1 << " and " << n2 << " is: " << lcm;
+    return 0;
+}
